14a93c34380431dd8a672c56c6b73a29a3ba4ade
yahaha <499421603@qq.com>
2022-09-08 19:44:31 +0800

    2

--- src/com/pzl/TestJava.java
+++ src/com/pzl/TestJava.java
@@ -3,5 +3,6 @@ package com.pzl;
 public class TestJava {
     public static void main(String[] args) {
         System.out.println("111111111111");
+        System.out.println("222222222222");
     }
 }
